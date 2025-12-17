de1319a8357f8fb08f70c2239804959dd7e77740
kneetate <hizatate.tomoaki.j8@elms.hokudai.ac.jp>
2025-12-17 10:08:40 +0900

    add support for different colors in draw_detections_on_image and update function signatures

--- yolo_ros_utils/include/yolo_ros_utils/yolo_ros_utils.hpp
+++ yolo_ros_utils/include/yolo_ros_utils/yolo_ros_utils.hpp
@@ -12,19 +12,20 @@
 namespace yolo_ros_utils
 {
 
-class YoloRosUtils
-{
-public:
-  YoloRosUtils();
-  static cv::Mat draw_detections_on_image(
-      const cv::Mat &image,
-      const yolo_msgs::msg::DetectionArray &detection_array);
-  static cv::Mat draw_detections_on_image(
-      const sensor_msgs::msg::Image &image_msg,
-      const yolo_msgs::msg::DetectionArray &detection_array);
-  virtual ~YoloRosUtils();
-};
+  class YoloRosUtils
+  {
+  public:
+    YoloRosUtils();
+    static cv::Mat draw_detections_on_image(
+        const cv::Mat &image,
+        const yolo_msgs::msg::DetectionArray &detection_array, bool different_color = false);
+    static cv::Mat draw_detections_on_image(
+        const sensor_msgs::msg::Image &image_msg,
+        const yolo_msgs::msg::DetectionArray &detection_array, bool different_color = false);
+    static std::vector<cv::Scalar> create_random_colors(const int &num_colors);
+    virtual ~YoloRosUtils();
+  };
 
-}  // namespace yolo_ros_utils
+} // namespace yolo_ros_utils
 
-#endif  // YOLO_ROS_UTILS__YOLO_ROS_UTILS_HPP_
+#endif // YOLO_ROS_UTILS__YOLO_ROS_UTILS_HPP_
--- yolo_ros_utils/src/yolo_ros_utils.cpp
+++ yolo_ros_utils/src/yolo_ros_utils.cpp
@@ -13,37 +13,64 @@ namespace yolo_ros_utils
 
     cv::Mat YoloRosUtils::draw_detections_on_image(
         const cv::Mat &image,
-        const yolo_msgs::msg::DetectionArray &detection_array)
+        const yolo_msgs::msg::DetectionArray &detection_array, bool different_color)
     {
         cv::Mat output_image = image.clone();
+        std::vector<cv::Scalar> colors = YoloRosUtils::create_random_colors(detection_array.detections.size());
+        int color_index = 0;
         for (const auto &detection : detection_array.detections)
         {
-            cv::Rect box(cv::Point(detection.bbox.center.position.x, detection.bbox.center.position.y),
+            cv::Scalar color = different_color ? colors[color_index++] : cv::Scalar(0, 255, 0);
+            cv::Rect box(cv::Point(detection.bbox.center.position.x - detection.bbox.size.x / 2, detection.bbox.center.position.y - detection.bbox.size.y / 2),
                          cv::Size(detection.bbox.size.x, detection.bbox.size.y));
-            cv::rectangle(output_image, box, cv::Scalar(0, 255, 0), 2);
+            cv::rectangle(output_image, box, color, 2);
             cv::Mat mask_mat = cv::Mat::zeros(output_image.rows, output_image.cols, CV_8UC3);
-            for (const auto &mask_pt : detection.mask.data)
+            //  detection.mask.dataに内包される領域点を塗りつぶす
+            std::vector<cv::Point> points_mat(detection.mask.data.size());
+            for (int i = 0; i < detection.mask.data.size(); ++i)
             {
-                mask_mat.at<cv::Vec3b>(static_cast<int>(mask_pt.y), static_cast<int>(mask_pt.x)) = cv::Vec3b(0, 255, 0);
+                if (i > 0)
+                {
+                    cv::line(mask_mat,
+                             cv::Point(detection.mask.data[i - 1].x, detection.mask.data[i - 1].y),
+                             cv::Point(detection.mask.data[i].x, detection.mask.data[i].y),
+                             color, 2);
+                }
+                points_mat[i] = cv::Point(detection.mask.data[i].x, detection.mask.data[i].y);
             }
+            cv::fillConvexPoly(mask_mat, points_mat, color, cv::LINE_AA);
             std::string label = detection.class_name + ": " + std::to_string(detection.score);
             int baseLine;
             cv::Size label_size = cv::getTextSize(label, cv::FONT_HERSHEY_SIMPLEX, 0.5, 1, &baseLine);
             cv::rectangle(output_image, cv::Point(box.x, box.y - label_size.height),
                           cv::Point(box.x + label_size.width, box.y + baseLine),
-                          cv::Scalar(0, 255, 0), cv::FILLED);
+                          color, cv::FILLED);
             cv::putText(output_image, label, cv::Point(box.x, box.y),
                         cv::FONT_HERSHEY_SIMPLEX, 0.5, cv::Scalar(0, 0, 0), 1);
-            cv::addWeighted(output_image, 1.0, mask_mat, 0.3, 0, output_image);
+            cv::addWeighted(output_image, 1.0, mask_mat, 0.8, 0, output_image);
         }
         return output_image;
     }
 
     cv::Mat YoloRosUtils::draw_detections_on_image(
         const sensor_msgs::msg::Image &image_msg,
-        const yolo_msgs::msg::DetectionArray &detection_array)
+        const yolo_msgs::msg::DetectionArray &detection_array, bool different_color)
     {
         cv::Mat cv_image = cv_bridge::toCvShare(std::make_shared<sensor_msgs::msg::Image>(image_msg), "bgr8")->image;
-        return draw_detections_on_image(cv_image, detection_array);
+        return draw_detections_on_image(cv_image, detection_array, different_color);
+    }
+
+    std::vector<cv::Scalar> YoloRosUtils::create_random_colors(const int &num_colors)
+    {
+        std::vector<cv::Scalar> colors;
+        cv::RNG rng(12345);
+        for (int i = 0; i < num_colors; ++i)
+        {
+            int r = rng.uniform(0, 256);
+            int g = rng.uniform(0, 256);
+            int b = rng.uniform(0, 256);
+            colors.push_back(cv::Scalar(b, g, r));
+        }
+        return colors;
     }
 } // namespace yolo_ros_utils
